038c6be0d8c764274ae554bf67f0126590f2ed63
djgithubdj <1165950150@qq.com>
2019-01-10 14:27:58 +0800

    第一次版本更新

--- gulp_project1/src/js/index.js
+++ gulp_project1/src/js/index.js
@@ -1,5 +1,34 @@
-require(["requirejs.config"], () => {
-	require(["jquery",  "header"], () => {
-		
+require(["./requirejs.config"], () => {
+	require(["jquery","header","footer"],($)=>{
+		function index(){
+			this.lbt();
+		}
+		$.extend(index.prototype,{
+			//轮播图功能
+			lbt:function(){
+				var index = 0;
+				//鼠标移动到点上的时候添加class名current，将其兄弟的class移除
+				$(".circle").on("mousemove","b",(event)=>{
+					var event = event.target;
+					$(event).addClass("current").siblings().removeClass("current");
+					let children = $(".circle")[0].children;
+					index = $(event).index();//下标与img一致
+//					console.log(index)
+					let img = $(".box")[0].children;
+//					console.log(img[index])
+					$(img[index]).show().siblings().hide();
+				})
+				//自动切换轮播图
+				var timer = setInterval(()=>{
+					let img = $(".box")[0].children;
+					let children = $(".circle")[0].children;
+					index++;
+					if(index>5)index=0;
+					$(img[index]).show().siblings().hide();
+					$(children[index]).addClass("current").siblings().removeClass("current");
+				},2000)
+			}
+		})
+		new index();
 	});
 });
